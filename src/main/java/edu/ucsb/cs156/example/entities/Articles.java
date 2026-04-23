1f7b9deee301bdd813e699fd40bcb07ede1c38dc
Amit Karthikeyan <amitkarthikeyan@ucsb.edu>
2026-04-22 21:05:58 -0700

    fixed entity formatting

--- src/main/java/edu/ucsb/cs156/example/entities/Articles.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Articles.java
@@ -6,10 +6,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
