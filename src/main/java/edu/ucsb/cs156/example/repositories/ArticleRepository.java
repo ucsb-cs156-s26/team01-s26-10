9450053de09d88e74350d0b99b7244d46f652b3f
Amit Karthikeyan <amitkarthikeyan@ucsb.edu>
2026-04-22 21:26:45 -0700

    fixed formatting

--- src/main/java/edu/ucsb/cs156/example/repositories/ArticleRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/ArticleRepository.java
@@ -1,10 +1,10 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.Articles;
+import edu.ucsb.cs156.example.entities.Article;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RepositoryRestResource(exported = false)
-public interface ArticleRepository extends CrudRepository<Articles, Long> {}
+public interface ArticleRepository extends CrudRepository<Article, Long> {}
