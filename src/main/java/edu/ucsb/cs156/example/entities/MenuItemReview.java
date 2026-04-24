6be424968824770c48382a5de96ca1f26f3abe05
Kazu <kazu@ucsb.edu>
2026-04-23 18:22:38 -0700

    Add entity class for MenuItemReview

---
+++ src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
@@ -0,0 +1,26 @@
+package edu.ucsb.cs156.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "menuitemreviews")
+public class MenuItemReview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String diningCommonsCode;
+    private String name;
+    private String station;
+}
